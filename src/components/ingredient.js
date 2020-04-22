2c0c3a4bb03e642a7fb143edf10f108f0e42c1c4
Antonio Branco <antoniobranco@sapo.pt>
2020-04-22 17:25:05 +0100

    SearchBarConcluded

--- src/components/ingredient.js
+++ src/components/ingredient.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import axios from 'axios';
 import IngredientsList from './IngredientsList';
-
+import SearchBar from'./SearchBar'
 
 class Ingredient extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
+    this.state = {  
       ingredientsList: [],
+      searchInputValue: '',
+      allIngredients: []
     };
   }
 
@@ -17,15 +19,46 @@ class Ingredient extends React.Component {
       .get(url)
       .then((response) => response.data.meals)
       .then((ingredientsData) => {
-        this.setState({ ingredientsList: ingredientsData });
+        this.setState({ ingredientsList: ingredientsData, allIngredients: ingredientsData });
       });
   }
+ handleChange=(event)=>{
+   this.setState({searchInputValue:event.target.value})
+ }
 
+ handleSubmit=()=>{
+   let inputValue=this.state.searchInputValue
+ let ingredientsListVariable= this.state.allIngredients
+ let findIngredients = ingredientsListVariable.filter(ingredient=> {
+   return ingredient.strIngredient.toLowerCase().includes(inputValue.toLowerCase())})
+   this.setState({ingredientsList:findIngredients})
+   console.log('submit');
+ }
+  /*{let searchInputValueVariable = this.state.searchInputValue
+  let ingredientsListVariable =this.state.ingredientsList.strDescription 
+  
+ingredientsListvariable.map(ingredient=>(
+ingredient.includes(searchInputValueVariable)
+this.state({ingredientsSearchResult:searchInputValueVariable})
+
+
+))
+
+  if (ingredientsListVariable.includes(searchInputValueVariable)){
+    this.setState({ingredientsSearchResult:ingredientsListVariable})
+  }
+}*/
 
   render() {
     const { ingredientsList } = this.state;
     return (
       <div>
+        <SearchBar 
+        input={this.state.searchInputValue}
+        inputChangeHandler={this.handleChange}
+        inputHandleSubmit={this.handleSubmit}
+        />
+
         <h1>Ingredients</h1>
         {ingredientsList.map((ingredient) => (
           <IngredientsList
