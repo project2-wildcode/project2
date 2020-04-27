ed47ec21142a6f3d941abe8842d2abd7d7a1e755
Antonio Branco <antoniobranco@sapo.pt>
2020-04-27 16:59:13 +0100

    first commit

--- src/components/RecipesList.js
+++ src/components/RecipesList.js
@@ -1,8 +1,8 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 function RecipeList({ name }) {
-  return <div>{name}</div>;
+	return <div>{name}</div>;
 }
 
 export default RecipeList;
