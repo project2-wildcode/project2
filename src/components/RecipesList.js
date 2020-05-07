93170f46e076d27ad9ca6c2af2aae0c8a17a662b
François ARBOGAST <arbogast.francois@gmail.com>
2020-05-07 10:20:13 +0100

    Recipe page with all infos

--- src/components/RecipesList.js
+++ src/components/RecipesList.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function RecipeList(props) {
-	const { showRecipes, name, selectRecipe } = props;
-	return <div onClick={() => selectRecipe(name)}><Link to='/recipe'>{showRecipes ? name : ''}</Link></div>;
+  const {
+    showRecipes,
+    name,
+    selectRecipe,
+    rating,
+    time,
+    level,
+    people,
+  } = props;
+  return (
+    <div onClick={() => selectRecipe(name, rating, time, level, people)}>
+      <Link to="/recipe">{showRecipes ? name : ""}</Link>
+    </div>
+  );
 }
 
 export default RecipeList;
