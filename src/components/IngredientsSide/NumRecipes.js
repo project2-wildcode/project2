d3a6b6aad334cce4bd0b1623066d25d68b7c8432
François ARBOGAST <arbogast.francois@gmail.com>
2020-05-13 11:07:57 +0100

    container right

--- src/components/IngredientsSide/NumRecipes.js
+++ src/components/IngredientsSide/NumRecipes.js
@@ -9,7 +9,7 @@ function NumRecipes(props) {
         <div className="search-info">
           <p>
             click the cards to add
-            <span>ingredients</span>
+            <span> ingredients</span>
           </p>
         </div>
       );
@@ -27,8 +27,8 @@ function NumRecipes(props) {
       return (
         <div className="search-info">
           <p>
-            <span>{numRecipes}</span>
-            {numRecipes === 1 ? "recipe" : "recipes"}
+            <span>{numRecipes} </span>
+            {numRecipes === 1 ? "recipe " : "recipes "}
             available
           </p>
         </div>
--- src/components/SharedComponents/RecipeList/RecipesList.js
+++ src/components/SharedComponents/RecipeList/RecipesList.js
@@ -1,51 +1,43 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import StarRatings from 'react-star-ratings';
-import { GiCookingPot } from 'react-icons/gi';
-import { MdPeople } from 'react-icons/md';
-import { AiFillClockCircle } from 'react-icons/ai';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import StarRatings from "react-star-ratings";
+import { GiCookingPot } from "react-icons/gi";
+import { MdPeople } from "react-icons/md";
+import { AiFillClockCircle } from "react-icons/ai";
+import "./RecipeList.scss";
 
 function RecipesList(props) {
-  const {
-    name,
-    selectRecipe,
-    rating,
-    time,
-    level,
-    people,
-    thumbnail,
-  } = props;
+  const { name, selectRecipe, rating, time, level, people, thumbnail } = props;
   return (
     <div
       className="recipes-list-card"
       onClick={() => selectRecipe(name, rating, time, level, people)}
     >
-      <Link to="/recipe">
-        <img src={thumbnail} alt={name} />
-        <div className="card-info">
-          <h2>{name}</h2>
-          <StarRatings
-            rating={rating}
-            starRatedColor="blue"
-            numberOfStars={5}
-          />
-          <div className="extra-info">
-            <div className="extra-info-wrapper">
-              <AiFillClockCircle className="icon" />
-              <p>{time}</p>
-            </div>
-            <div className="extra-info-wrapper">
-              <MdPeople className="icon" />
-              <p>{people}</p>
-            </div>
-            <div className="extra-info-wrapper">
-              <GiCookingPot className="icon" />
-              <p>{level}</p>
-            </div>
+      <img src={thumbnail} alt={name} />
+      <div className="card-info">
+        <h2>{name}</h2>
+        <StarRatings
+          rating={rating}
+          starRatedColor="#F7C233"
+          numberOfStars={5}
+          starDimension="20px"
+        />
+        <div className="extra-info">
+          <div className="extra-info-wrapper">
+            <AiFillClockCircle className="icon" />
+            <p>{time}</p>
+          </div>
+          <div className="extra-info-wrapper">
+            <MdPeople className="icon" />
+            <p>{people}</p>
+          </div>
+          <div className="extra-info-wrapper">
+            <GiCookingPot className="icon" />
+            <p>{level}</p>
           </div>
         </div>
-      </Link>
+      </div>
     </div>
   );
 }
