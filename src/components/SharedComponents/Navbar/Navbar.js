e9f0c8393a8686aab28b7095ba217b96bb203fab
Tiago Baptista Coelho <tiagojnbaptistacoelho@gmail.com>
2020-05-13 16:14:50 +0100

    made navbar al responsive and ingredients component half responsive

--- src/components/SharedComponents/Navbar/Navbar.js
+++ src/components/SharedComponents/Navbar/Navbar.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../styles/logo/logo.svg';
 import './Navbar.scss';
+import { IoMdClose, IoIosMenu } from 'react-icons/io';
+
 
 
 function Navbar() {
+
+  const [navExpand, setNavExpand] = useState(false);
+
+  const toggleNavExpand = () => {
+    setNavExpand(!navExpand);
+  };
+
   return (
-    <div className="navbar">
-      <Link to="/"><Logo className="navbar-logo" /></Link>
+    <div className={navExpand ? "navbar-expand" : "navbar"}>
+      {!navExpand ? <IoIosMenu className="burger-menu" onClick={toggleNavExpand} /> : <IoMdClose className="burger-menu" onClick={toggleNavExpand} /> }
       <ul className="navbar-items">
-        <li><Link to="/ingredients" className="navbar-link">Ingredients</Link></li>
-        <li><Link to="/recipes" className="navbar-link">Recipes</Link></li>
+        <li><Link to="/ingredients">Ingredients</Link></li>
+        <li><Link to="/recipes">Recipes</Link></li>
       </ul>
     </div>
   );
