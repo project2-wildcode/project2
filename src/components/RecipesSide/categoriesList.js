1dcc5c5b9b3dee2a45741e59a6b101ab87530659
Antonio Branco <antoniobranco@sapo.pt>
2020-05-13 16:20:15 +0100

    Recipes desktop CSS

--- src/components/RecipesSide/categoriesList.js
+++ src/components/RecipesSide/categoriesList.js
@@ -1,5 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Recipes.scss";
+
 
 function CategoriesList(props) {
   const { name, thumbnail, getRecipesByCat } = props;
