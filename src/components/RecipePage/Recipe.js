068755e3365a08c926f9335a74c1b64d5089c849
François ARBOGAST <arbogast.francois@gmail.com>
2020-05-13 15:08:30 +0100

    changes

--- src/components/RecipePage/Recipe.js
+++ src/components/RecipePage/Recipe.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import StarRatings from 'react-star-ratings';
-import { GiCookingPot } from 'react-icons/gi';
-import { MdPeople, MdArrowBack } from 'react-icons/md';
-import { AiFillClockCircle } from 'react-icons/ai';
-import { IoIosArrowUp, IoIosArrowDown } from 'react-icons/io';
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import StarRatings from "react-star-ratings";
+import { GiCookingPot } from "react-icons/gi";
+import { MdPeople, MdArrowBack } from "react-icons/md";
+import { AiFillClockCircle } from "react-icons/ai";
+import { IoIosArrowUp, IoIosArrowDown } from "react-icons/io";
+import "./Recipe.scss";
 
 function strManipulation(str) {
-  return str.replace(/(\r\n|\n|\r)/gm, '').split('. ');
+  return str.replace(/(\r\n|\n|\r)/gm, "").split(". ");
 }
-
 
 class Recipe extends Component {
   constructor(props) {
@@ -24,26 +24,21 @@ class Recipe extends Component {
   componentDidMount() {
     const { location } = this.props;
     const { state } = location;
-    const {
-      name,
-      rating,
-      time,
-      level,
-      people,
-      from,
-    } = state;
+    const { name, rating, time, level, people, from } = state;
     const url = `https://www.themealdb.com/api/json/v2/9973533/search.php?s=${name}`;
     axios
       .get(url)
       .then((response) => response.data.meals[0])
       .then((recipeData) => {
-        recipeData.strInstructions = strManipulation(recipeData.strInstructions);
+        recipeData.strInstructions = strManipulation(
+          recipeData.strInstructions
+        );
         const newInfo = {
           recipeInfoRating: rating,
           perpTime: time,
           difficulty: level,
           numOfPeople: people,
-          path: from
+          path: from,
         };
         const recipeUpdatedData = { ...recipeData, ...newInfo };
         this.setState({ recipeInfo: recipeUpdatedData });
@@ -56,9 +51,19 @@ class Recipe extends Component {
     for (let i = 1; i <= 20; i += 1) {
       const strIngredient = `strIngredient${i}`;
       const strMeasure = `strMeasure${i}`;
-      if (recipeInfo[strIngredient] !== '' && recipeInfo[strIngredient] !== ' ' && recipeInfo[strIngredient] !== null) {
-        if (recipeInfo[strMeasure] !== '' && recipeInfo[strMeasure] !== ' ' && recipeInfo[strMeasure] !== null) {
-          ingredients.push(`${recipeInfo[strIngredient]} (${recipeInfo[strMeasure]}) `);
+      if (
+        recipeInfo[strIngredient] !== "" &&
+        recipeInfo[strIngredient] !== " " &&
+        recipeInfo[strIngredient] !== null
+      ) {
+        if (
+          recipeInfo[strMeasure] !== "" &&
+          recipeInfo[strMeasure] !== " " &&
+          recipeInfo[strMeasure] !== null
+        ) {
+          ingredients.push(
+            `${recipeInfo[strIngredient]} (${recipeInfo[strMeasure]}) `
+          );
         } else {
           ingredients.push(recipeInfo[strIngredient]);
         }
@@ -69,21 +74,20 @@ class Recipe extends Component {
 
   render() {
     const { recipeInfo } = this.state;
-    console.log(recipeInfo.strInstructions)
+    console.log(recipeInfo.strInstructions);
 
     return (
       <div className="page-wrapper">
         <div className="recipeInfo-left-container">
-          {recipeInfo.path === 'ingredients' ? (
+          {recipeInfo.path === "ingredients" ? (
             <Link to="/ingredients">
               <MdArrowBack className="go-back" />
             </Link>
-          )
-            : (
-              <Link to="/recipeInfos">
-                <MdArrowBack className="go-back" />
-              </Link>
-            )}
+          ) : (
+            <Link to="/recipeInfos">
+              <MdArrowBack className="go-back" />
+            </Link>
+          )}
           <div className="recipeInfo-card">
             <img src={recipeInfo.strMealThumb} alt={recipeInfo.strMeal} />
             <div className="recipeInfo-info">
@@ -129,27 +133,26 @@ class Recipe extends Component {
         <div className="recipeInfo-container-right">
           <div className="instructions-title-wrapper">
             <h2 className="tab-title">Instructions:</h2>
-            { /* bottomTabExpanded ? <IoIosArrowDown className="toggle-icon" onClick={toggleBottomTab} /> : <IoIosArrowUp className="toggle-icon" onClick={toggleBottomTab} /> */ }
+            {/* bottomTabExpanded ? <IoIosArrowDown className="toggle-icon" onClick={toggleBottomTab} /> : <IoIosArrowUp className="toggle-icon" onClick={toggleBottomTab} /> */}
           </div>
           <div className="instructions-wrapper-right">
             <h2 className="instructions-title">Instructions</h2>
-            {recipeInfo.strInstructions !== undefined
-              && (
-                recipeInfo.strInstructions.map((instruction, index) => (
-                  <p className="instructions">
-                    <span>
-                      {index + 1}
-                      .
-                    </span>
-                    {' '}
-                    {instruction}
-                  </p>
-                )))}
+            {recipeInfo.strInstructions !== undefined &&
+              recipeInfo.strInstructions.map((instruction, index) => (
+                <p className="instructions">
+                  <span>{index + 1}.</span> {instruction}
+                </p>
+              ))}
             <h2 className="video-title">Video:</h2>
-            <a href={recipeInfo.strYoutube} target="_blank" rel="noopener noreferrer">click to get instructions on video</a>
+            <a
+              href={recipeInfo.strYoutube}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              click to get instructions on video
+            </a>
           </div>
         </div>
-
       </div>
     );
   }
